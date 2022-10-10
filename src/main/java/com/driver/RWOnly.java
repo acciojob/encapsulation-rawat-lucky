968dbf0b8e6f47137f7af1f210c6022b2d776b02
rawat-lucky <108919710+rawat-lucky@users.noreply.github.com>
2022-10-10 17:15:54 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,5 +2,11 @@ package com.driver;
 
 public class RWOnly {
     Private String name;
-    
+  String  get(){
+      return this.name;
+    }
+    void set(String nm){
+        name=nm;
+    }
+        
 }
