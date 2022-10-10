a41ed077ae0da2965f4b4760b53df9bd9cfcd3ea
rawat-lucky <108919710+rawat-lucky@users.noreply.github.com>
2022-10-10 17:20:03 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,7 +5,8 @@ public class Main {
   public static void main(String args[]){
      RWOnly obj=new RWOnly();
   //  obj.name="manish";
-    
+   // unresolved compilation problem 
+   // THE field RWOnly.name is not visble
     obj.set("aditya");
     System.out.println(obj.get());
   }
